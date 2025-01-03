35c68a7bc88e9661aeadb2e65c50abeba99a677c
AAMINA BIBI <aaminabibi2324@gmail.com>
2025-01-03 17:32:27 +0500

    datatype commit

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,11 @@
+//number ->2 to power of 53
+//bigint
+//string ->"
+//boolean->true/false"
+//null=>standalone value=>object
+//undefines=>undefind
+//symbol =>unique
+
+console.log(typeof undefined);
+console.log(typeof null);
+
