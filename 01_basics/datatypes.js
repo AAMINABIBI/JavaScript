c9b164639cded7de7c0cb0a9b5aa7c38d4eaef94
AAMINA BIBI <aaminabibi2324@gmail.com>
2025-01-03 18:56:34 +0500

    conversion of datatype

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -9,3 +9,30 @@
 console.log(typeof undefined);
 console.log(typeof null);
 
+//conversions of  operation
+//string to number
+let score="33";
+let newNum=Number(score)
+console.log(score);
+console.log(typeof(score))
+console.log(newNum)
+console.log(typeof(newNum))
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1,false=>0
+
+
+//nuber to bool
+const isLoggedIn=1
+let boolLoggedin=Boolean(isLoggedIn)
+console.log(isLoggedIn)
+console.log(boolLoggedin)
+//1=>true,0=>false
+//""=>false
+//"aman"=>true
+
+//numbber to string
+let number=33
+let stringNum=String(number)
+console.log(typeof stringNum)
