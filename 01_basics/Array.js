40e9ff0b10e9e397cf97902040254e6beb25052b
AAMINA BIBI <aaminabibi2324@gmail.com>
2025-01-05 17:15:21 +0500

    Array updated

--- 01_basics/Array.js
+++ 01_basics/Array.js
@@ -23,3 +23,30 @@ const new2=myArr.splice(1,3)
 //splice will add te range and will remove the selected splice from the array
 console.log(new2)
 console.log("C:" ,myArr)
+
+
+
+//combining 2 arrays
+const arr1=["one","two","three"]
+const arr2=["four","five","six"]
+
+arr1.push(arr2);
+console.log(arr1)//will ad arr2 as suarray on 3 index to access its element arr1[3][2]
+
+//concat
+const newArr=arr1.concat(arr2)
+console.log(newArr)
+//will join both array
+
+//spread operator
+//will spit the whole Array
+
+const newArray=[...arr1,...arr2]
+console.log(newArray)
+
+
+console.log(Array.isArray("amna"))//will tell weather it is array or not
+
+console.log(Array.from("amna"))//convert this string to array
+
+
