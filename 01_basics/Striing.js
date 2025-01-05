872c22598d7d0402a0fbd99fd6e23489ab7e5067
AAMINA BIBI <aaminabibi2324@gmail.com>
2025-01-05 11:47:39 +0500

    String Commit

---
+++ 01_basics/Striing.js
@@ -0,0 +1,20 @@
+//string interplotation=> use of bacticks(``)
+
+//string declaration
+let name="amna"
+
+let name2=new String("haleema")
+//objecct=>key-value pair=>0-h,1-a,2-l
+
+name2.length
+name2.toLocaleLowerCase()//function
+name2.charAt(2)//character at 2 position
+name2.indexOf('a')
+
+//trim=>remove extra white spaces\
+//substring=>returns a substring
+//slice=>return substring but also with -ve values(reverse direction)
+//replace
+//includes=>return bool whether a certain word is present in string or not
+//split
+//check MDN website
