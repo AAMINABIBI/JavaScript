cb542eb6a4ed2989b1b183184111df5308141176
AAMINA BIBI <aaminabibi2324@gmail.com>
2025-01-08 13:48:39 +0500

    Functions commit

---
+++ 01_basics/Functions.js
@@ -0,0 +1,38 @@
+function saymyName(){
+    console.log("amna")
+}
+
+saymyName();
+
+
+
+function addTwoNum(num1,num2){
+    return num1+num2
+}
+
+
+const num3=addTwoNum(3,4)
+console.log(num3)
+
+
+
+function calculateCadPrice(...num1)//rest operator
+{
+    return num1
+}
+
+
+console.log(calculateCadPrice(200,4,455,453,))
+
+//rest operator
+//when we don't know the number of arguments from 
+// function,we use rest operator which will jsut bundle
+//  all the elements is an array
+
+//finnction svope
+//loacl scope
+//global scope
+//issue of var variable
+//arrow fuction
+//this keyword
+
