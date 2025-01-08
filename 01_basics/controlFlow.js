a41448b1d6b49b7ce2b3d46c060d4e8ce952c44e
AAMINA BIBI <aaminabibi2324@gmail.com>
2025-01-08 14:35:37 +0500

    Control Flow Commit

---
+++ 01_basics/controlFlow.js
@@ -0,0 +1,34 @@
+if(true){
+
+}
+else if()
+{
+
+}
+else{
+
+}
+
+//comparison
+//== & ===
+//variable scope=?Block Scope
+//&& or ||
+
+
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+//falsy Values=>false ,0,-0,"",null,undefined.NaN
+//truthy Values=> other than flsy values,"0",'false',[]empty arryry,{}empty object
+
+
+//Nullish Coalescing operator=>(??)
+//Ternary Operator=>(?)
+
